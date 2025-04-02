490e59605fcf055d4e1ba38f767d0cfa2f8cb427
Finn Haker <schaukelei@gmail.com>
2025-04-02 20:16:02 +0200

    first commit

---
+++ app-files/data.js
@@ -0,0 +1,62 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20250401_084800_462",
+      "name": "IMG_20250401_084800_462",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2725755003669512,
+          "pitch": 0.06107139922246674,
+          "rotation": 0,
+          "target": "0-img_20250401_084800_462"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3790814958093627,
+          "pitch": -0.21959911886756522,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Clemens</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Das ist Clemens. Er ist ein sehr smashy sigma Boy.</font></font>"
+        },
+        {
+          "yaw": -0.027719164241434058,
+          "pitch": -0.2371461976945799,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Finn</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Das ist Finn. Ein 14-jähriger Junge.</font></font>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
